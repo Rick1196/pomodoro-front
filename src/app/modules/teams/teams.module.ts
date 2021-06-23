6c18ef8fb7e553ed4c7b2dae3477846c5121fa61
ricardomanuel-perez <ricardo.perez.mx@schilleramercias.com>
2021-06-22 23:34:25 -0500

    :zap: Teams dashabord components v1

--- src/app/modules/teams/teams.module.ts
+++ src/app/modules/teams/teams.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TeamsRoutingModule } from './teams-routing.module';
 import { TeamsDashboardComponent } from './component/teams-dashboard/teams-dashboard.component';
+import { CreateTeamComponent } from './component/create-team/create-team.component';
 
 
 @NgModule({
   declarations: [
     TeamsDashboardComponent,
+    CreateTeamComponent,
   ],
   imports: [
     CommonModule,
