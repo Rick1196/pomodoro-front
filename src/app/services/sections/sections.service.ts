411251dfbaf98390707c525a8f949512faee5c50
Rick1196 <34297467+Rick1196@users.noreply.github.com>
2021-06-30 00:00:14 -0500

    :zap:  Section service

---
+++ src/app/services/sections/sections.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SectionI } from 'src/app/interfaces/section';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SectionsService {
+  constructor(public afs: AngularFirestore) {}
+
+  public createSection(sections: SectionI) {
+    this.afs.collection('sections').add(sections);
+  }
+
+  public readTeamSections(teamId: string): Observable<SectionI[]> {
+    return this.afs
+        .collection<SectionI>('sections', (ref) => {
+          return ref.where('teamId', '==', teamId );
+        })
+        .snapshotChanges().pipe(
+          map(changes =>
+            changes.map(c =>
+              ({ uid: c.payload.doc.id, ...c.payload.doc.data() })
+            )
+          )
+        );
+  }
+}
