02ae815d62d1c452643e4ff7b0022b0bd6c59be7
Rick1196 <ricardompp11@gmail.com>
2021-06-29 20:33:44 -0500

    :zap: Menu icon with transition

--- src/app/component/shared/side-menu/side-menu.component.ts
+++ src/app/component/shared/side-menu/side-menu.component.ts
@@ -6,18 +6,9 @@ import { SideMenuService } from 'src/app/services/side-menu/side-menu.service';
   templateUrl: './side-menu.component.html',
 })
 export class SideMenuComponent implements OnInit {
-  public containerClass:string = 'left-side closed';
   constructor(
     public menuService: SideMenuService
   ) { 
-    this.menuService.menuStatusObservable.subscribe({
-      next: (status:boolean) => {
-        this.containerClass = (status)?'left-side closed':'left-side'
-      },
-      error: (error)=>{
-        console.error(error);
-      }
-    })
   }
 
   ngOnInit(): void {
