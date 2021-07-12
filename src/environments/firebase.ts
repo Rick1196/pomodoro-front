c745bc41229639166f6cbfd04ffd23668d86c036
Rick1196 <34297467+Rick1196@users.noreply.github.com>
2021-07-11 23:05:59 -0500

    :wrench: Github actions conf

---
+++ src/environments/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDrogY3wUS715zKTok2v3CVfgoxXaFzUKM',
+  authDomain: 'pomodoro-app-44c9b.firebaseapp.com',
+  databaseURL: 'https://pomodoro-app-44c9b-default-rtdb.firebaseio.com',
+  projectId: 'pomodoro-app-44c9b',
+  storageBucket: 'pomodoro-app-44c9b.appspot.com',
+  messagingSenderId: '289857841694',
+  appId: '1:289857841694:web:c474d570809171d37bdc0f',
+  measurementId: 'G-23394CXF2R',
+};
